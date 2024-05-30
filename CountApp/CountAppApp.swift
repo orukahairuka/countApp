184fe310b21aa3b49a332adcb47383574e33dc27
orukahairuka <sakuraierika03301109@gmail.com>
2024-05-30 21:35:59 +0900

    Initial Commit

---
+++ CountApp/CountAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountAppApp.swift
+//  CountApp
+//
+//  Created by 櫻井絵理香 on 2024/05/30.
+//
+
+import SwiftUI
+
+@main
+struct CountAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
