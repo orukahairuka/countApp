c8349c799099fdbd9902cfb9b84caed41d5c7d04
irukahaoruka <erika03301109@gmail.com>
2024-06-01 21:39:04 +0900

    ちょっと触った

--- CountApp/CountAppApp.swift
+++ CountApp/CountAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CountAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TimerSet()
         }
     }
 }
---
+++ CountApp/TimerSet.swift
@@ -0,0 +1,31 @@
+//
+//  TimerSetView.swift
+//  CountApp
+//
+//  Created by 櫻井絵理香 on 2024/06/01.
+//
+
+import SwiftUI
+
+struct TimerSet: View {
+    @State private var selectionDate = Date()
+    var body: some View {
+        VStack {
+            /// ラベル
+            HStack {
+                Image(systemName: "timer")
+                Text("日時設定")
+            }
+            .font(.title)
+            .bold()
+            /// DatePicker
+            DatePicker("出国日時", selection: $selectionDate)
+                .labelsHidden()
+        }
+    }
+}
+
+#Preview {
+    TimerSet()
+        .environment(\.locale, Locale(identifier: "ja_JP"))    // 追加
+}
